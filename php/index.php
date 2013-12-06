724fd297b53a5ad7b63935755bc7e2dbde5a06c7
Abraham Lopez <alopez.cesun1013@gmai.com>
2013-12-06 15:34:00 -0800

    0

--- php/index.php
+++ php/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE HTML>
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
 	<title>Abraham Lopez Alvarez Resume</title>
 		<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
