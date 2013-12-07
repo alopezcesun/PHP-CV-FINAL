7e4fb0fd97289d45b943c9d12b09b91040f18016
Abraham Lopez <alopez.cesun1013@gmai.com>
2013-12-07 03:11:00 -0800

    0

--- php/index.php
+++ php/index.php
@@ -1,5 +1,6 @@
-<!DOCTYPE HTML>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<title>Abraham Lopez Alvarez Resume</title>
 		<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
